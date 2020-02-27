fbe827fe1880f23f9681c4047ca414687851a091
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2020-02-26 19:30:20 -0700

    Update rating data type

--- lib/models/entry.dart
+++ lib/models/entry.dart
@@ -1,7 +1,7 @@
 class Entry {
   final String title;
   final String description;
-  final double rating;
+  final int rating;
   final DateTime dateTime;
 
   const Entry({this.title, this.description, this.rating, this.dateTime});
